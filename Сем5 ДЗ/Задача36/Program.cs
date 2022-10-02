ea8f1b6ef70915be7175fb734a87bd34feb7f863
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-02 20:42:07 +0300

    Задача36

---
+++ Сем5 ДЗ/Задача36/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+Random rand = new Random();
+int length = rand.Next(5, 7);
+int[] array = CreateArray(length);
+int sum = SumUneven(array);
+PrintArray(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях равна {sum}");
+
+int[] CreateArray (int length)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(-50, 50);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == array.Length - 1)
+        {
+            Console.Write("]");
+        }
+        else
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+int SumUneven (int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        sum = sum + array[i];
+        i++;
+    }
+    return sum;
+}
