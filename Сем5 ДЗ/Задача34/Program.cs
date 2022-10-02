391e658d62d08ac34c54d58c598b8faac2548d47
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-02 20:31:26 +0300

    Задача34

---
+++ Сем5 ДЗ/Задача34/Program.cs
@@ -0,0 +1,52 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+Random rand = new Random();
+int length = rand.Next(2, 5);
+int[] array = CreateArray(length);
+int count = Even(array);
+PrintArray(array);
+Console.WriteLine($"Количество четных элементов в массиве равно {count}");
+
+int[] CreateArray (int length)
+{
+    int[] array = new int[length];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == length - 1)
+        {
+            Console.Write("]");
+        }
+        else
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+int Even (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
