fb31ccd5c9afd98b2c5b71330cf7a7a356fc0d97
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-09 16:33:10 +0300

    Задача1(звездочка)

---
+++ Сем6 ДЗ/Задача1(звездочка)/Program.cs
@@ -0,0 +1,17 @@
+﻿//Написать программу, которая рекурсивным методом переводит числа из десятичной системы счисления в двоичную
+//2 -> 10
+//11 -> 1011
+//16 -> 10000
+
+Console.Clear();
+
+Console.Write($"Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {number} в двоичной системе исчисления: {Interpretation(number)}");
+
+
+int Interpretation (int number)
+{
+    if (number == 0) return 0;
+    else return (number % 2 + 10 * Interpretation(number / 2));
+}
