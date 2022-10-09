9dd7548d57dce9cf12a0c2122cd9d9858c1f5f31
EkaterinaZhelannikova <katya.zhelannikova@mail.ru>
2022-10-09 23:55:40 +0300

    Задача47

---
+++ Сем7 ДЗ/Задача47/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.Clear();
+ 
+double[,] array = new double[3, 4];
+FillArray(array);
+PrintArray(array);
+ 
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+double[,] FillArray(double[,] array)
+{
+    Random generator = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = generator.Next(-10, 10) + Math.Round(generator.NextDouble(), 1);
+        }
+    }
+    return array;
+}
+
